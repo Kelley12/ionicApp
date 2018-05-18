4c81e45099a750800eb26a5ff5532c032260c10b
Blake <blakekelley127@gmail.com>
2018-05-17 21:33:44 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase';
+import { SigninPage } from '../pages/signin/signin';
+
+const config = {
+  apiKey: 'AIzaSyAw7zS5TyBBmwgxpx6YXS05c6a98q4XPVU',
+  authDomain: 'chatapp-6d67e.firebaseapp.com',
+  databaseURL: 'https://chatapp-6d67e.firebaseio.com/',
+  projectId: 'chatapp-6d67e',
+  storageBucket: 'gs://chatapp-6d67e.appspot.com',
+};
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = SigninPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    firebase.initializeApp(config);
+  }
+}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component, ViewChild } from '@angular/core';
+import { NavController, NavParams, Content } from 'ionic-angular';
+import { RoomPage } from '../room/room';
+import * as firebase from 'Firebase';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  }
+
+  joinChat() {
+    this.navCtrl.setRoot(RoomPage);
+  }
+}
